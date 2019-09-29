38587f30ea265fe8b64e08109375bc58d6d3a46e
Kota Nishinaka <private2030kota@gmail.com>
2019-09-29 11:24:04 +0900

    create main-concepts folder and helloworld component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import RoutingExample from './components/code-separation/routing-example';
+import HelloWorld from './components/main-concepts/hello-world.component';
 
 function App() {
   return (
     <div className='App'>
-      <RoutingExample />
+      <HelloWorld />
     </div>
   );
 }
