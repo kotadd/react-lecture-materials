8d66877d1fbae24be2c37b1bef97ff253690d80b
Kota Nishinaka <private2030kota@gmail.com>
2019-09-29 11:51:35 +0900

    Add conditional-rendering example

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 import RoutingExample from './components/code-separation/routing-example';
 import HelloWorld from './components/main-concepts/hello-world.component';
-import Clock from './components/main-concepts/state-props-lifecycle.component';
+import Clock from './components/main-concepts/state-props-lifecycle/clock.component';
+import LoginControl from './components/main-concepts/conditional-rendering/login-control.component';
 
 function App() {
   return (
     <div className='App'>
-      <Clock />
+      <LoginControl />
     </div>
   );
 }
