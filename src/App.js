a8fd84911858ea895dc03118cd1c518576416dad
Kota Nishinaka <private2030kota@gmail.com>
2019-09-29 11:36:36 +0900

    Add state-props-lifecycle example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 import RoutingExample from './components/code-separation/routing-example';
 import HelloWorld from './components/main-concepts/hello-world.component';
+import Clock from './components/main-concepts/state-props-lifecycle.component';
 
 function App() {
   return (
     <div className='App'>
-      <HelloWorld />
+      <Clock />
     </div>
   );
 }
