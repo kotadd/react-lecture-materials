8f64b1433307f8d700195ed3bb4100e11a4cc561
Kota Nishinaka <private2030kota@gmail.com>
2019-09-29 11:15:53 +0900

    code-separation by routing units

---
+++ src/components/code-separation/routing-example.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+
+const Home = lazy(() => import('./routes/home.component'));
+const About = lazy(() => import('./routes/about.component'));
+
+const RoutingExample = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default RoutingExample;
